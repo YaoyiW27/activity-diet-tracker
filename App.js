1a7ffebf8728e8426a6a2eeb17ca5256661d366a
YaoyiW27 <rmwyyy@gmail.com>
2024-09-28 16:53:58 -0700

    created screens and components files and set up navigation

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function BottomTabNavigator() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Activities" component={Activities} />
+      <Tab.Screen name="Diet" component={Diet} />
+    </Tab.Navigator>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={BottomTabNavigator} />
+        <Stack.Screen name="Activities" component={Activities} />
+        <Stack.Screen name="Diet" component={Diet} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
