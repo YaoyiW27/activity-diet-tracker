67c9468c0f070ba53e3f2d51dc301f6c11213d4b
YaoyiW27 <rmwyyy@gmail.com>
2024-09-30 23:07:11 -0700

    added background color for both main screens

--- Screens/Diet.js
+++ Screens/Diet.js
@@ -29,6 +29,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center', 
     padding: 10, 
+    backgroundColor: '#a3b18a'
   },
   addButton: {
     backgroundColor: '#d4a373', 
